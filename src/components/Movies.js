b6e550d464ffc6055ade2db7fec0a19e31cbff7f
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-01 13:35:24 +0300

    Completed Assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const Movies = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Runtime: {movie.time} min.</p>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+  return <div className="navbar">
+    <h1>Movies Page</h1>
+    {Movies}
+    </div>
+
+  
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return( <div className="navbar">
+    <div className='navbar'>
+      <ul>
+      <li><NavLink to='/'>Home</NavLink> <br /></li>
+      <li><NavLink to='/movies'>Movies</NavLink> <br /></li>
+      <li><NavLink to='/directors'>Directors</NavLink> <br /></li>
+      <li><NavLink to='/actors'>Actors</NavLink></li>
+      </ul>
+    </div>
+  </div>);
 }
 
 export default NavBar;
